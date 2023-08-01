46007213bcba8fdf0ee6ada711c6ec4edaa3bb5c
TogrulEminov <togrul40@mail.ru>
2023-08-01 14:03:12 +0400

    change

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Select relevant HTML elements
 const filterButtons = document.querySelectorAll("#filter-buttons button");
 const filterableCards = document.querySelectorAll("#filterable-cards .card");
 
-// Function to filter cards based on filter buttons
 const filterCards = (e) => {
     document.querySelector("#filter-buttons .active").classList.remove("active");
     e.target.classList.add("active");
